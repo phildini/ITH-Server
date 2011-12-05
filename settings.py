d09befe6ba7a9d9e26ebdff4665a4f9a555ad93d
Philip James <pj@insertteamhere.com>
2011-12-05 22:31:51 +0000

    Updated registration_form.html

--- settings.py
+++ settings.py
@@ -118,7 +118,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration_defaults',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
