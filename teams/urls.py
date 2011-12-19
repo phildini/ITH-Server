09b136c8b6c31b63ded0126ed093425842c8cf6e
Philip James <pj@insertteamhere.com>
2011-12-19 01:39:52 +0000

    Added some test URLs.

--- teams/urls.py
+++ teams/urls.py
@@ -9,6 +9,10 @@ urlpatterns = patterns('teams.views',
 	url(r'^about/', 'about'),
 	url(r'^(?P<team_id>\d+)/join/$', 'join'),
 	url(r'^(?P<team_id>\d+)/delete/$', 'delete'),
+	url(r'^(?P<team_id>\d)/(?P<project_id>\d)/$', 'project'),
+        url(r'^(?P<team_id>\d)/(?P<project_id>\d)/edit/$', 'project_edit'),
+        url(r'^(?P<team_id>\d)/add/$', 'project_add'),
+        url(r'^(?P<team_id>\d)/(?P<project_id>\d)/delete/$', 'project_del'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
